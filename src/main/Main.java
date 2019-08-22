bb2dda821aa00c84b8e4f12372f7745b85be146c
DELL <manav.singh_cs18@gla.ac.in>
2019-08-22 15:01:03 +0530

    add file Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * @author Manavsingh
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner val = new Scanner(System.in);
+        String n;
+        System.out.println("Enter the string");
+        n = val.next();
+        System.out.println("Welcome, " + n);
+
+    }
+}
