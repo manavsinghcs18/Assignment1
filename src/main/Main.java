6fb336c589b17becee396475daee81400568e468
DELL <manav.singh_cs18@gla.ac.in>
2019-08-22 15:32:26 +0530

    final change

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.nextLine().trim();
-        System.out.println("Welcome," + userName + "!");
-        Scanner.close();
-
+        System.out.println("Welcome, " + userName + "!");
+        System.out.println("apun n khatam kar dia..");
+        scanner.close();
     }
 }
